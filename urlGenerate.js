d00091e93d2f7832ad7b06bc9ae9f1958410ac34
Judd Gledhill <juddrgledhill@gmail.com>
2013-06-19 19:09:01 -0400

    adding in structure to handle JSON set
    JSON set for multiple URLS in a POST, URL logic for generating short
    name

---
+++ urlGenerate.js
@@ -0,0 +1,19 @@
+var counter = 0;
+
+function generateUrl(completeURL){
+    console.log("generating url for " + completeURL);
+	var computedURL = counter;
+	counter++;
+	
+	//TODO: return the generated URL somehow
+    return computedURL;
+}
+
+
+
+
+
+/////////////// EXPORTS ///////////////////////
+//just export each function that we want to call
+exports.generateUrl = generateURL;
+/////////////// EXPORTS ///////////////////////
