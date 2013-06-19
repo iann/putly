3efffeed1579fd5659e00333fddd927aac820c82
Jonathan Kaufman <jonathanjkaufman@gmail.com>
2013-06-19 18:29:31 -0400

    Mongo Connection

--- urlData.js
+++ urlData.js
@@ -1,6 +1,22 @@
 //data structure to hold url
 var urlMap = {};
 
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('putly', server);
+
+db.open(function (err, db) {
+    if (!err) {
+        console.log("Connected to 'putly' database");
+       
+    }
+});
+
 function getUrl(shortName){
     console.log("Fetching url for " + shortName);
     //TODO: update lastHitTime and hits 
