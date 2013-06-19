3effe385d911cc2b213d1f5dea099f9b5dacecf4
Ian Moriarty <rominet@gmail.com>
2013-06-19 17:59:53 -0400

    fixing indentation

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,39 +2,39 @@ var data = require('./urlData');
 
 /////////////// REQUEST HANDLERS ///////////////////////
 function setlink(request, response){
-	console.log("Request handler ’SETLINK’ was called.");
-	
-	//farm the params
-	var shortname = request.params.shortname;
-	var completeURL = request.params.completeurl;
-	var added = data.addUrl(shortname,completeURL);
-	console.log("INPUT: " + shortname + " : " + completeURL+" was added " + added);
-	
-	//TODO: do something useful
+    console.log("Request handler ’SETLINK’ was called.");
+    
+    //farm the params
+    var shortname = request.params.shortname;
+    var completeURL = request.params.completeurl;
+    var added = data.addUrl(shortname,completeURL);
+    console.log("INPUT: " + shortname + " : " + completeURL+" was added " + added);
+    
+    //TODO: do something useful
     response.end("link set to " + completeURL);
-	return;
+    return;
 }
 
 function getlinks(request, response){
-	console.log("Request handler ’GETLINKS’ was called.");
-	
-	
+    console.log("Request handler ’GETLINKS’ was called.");
+    
+    
     response.json(data.getAll());
-	
+    
 }
 
 
 function sendredirect(request, response){
-	console.log("Request handler ’SENDREDIRECT’ was called.");
-	
-	//farm the params
-	var shortname = request.params.shortname;
-	var urlData = data.getUrl(shortname);
-	console.log(urlData.url);
-	response.redirect(urlData.url);
-	//TODO: do something useful
-	//response.end("consider yourself redirected to " + redirectUrl + "...");
-	return;
+    console.log("Request handler ’SENDREDIRECT’ was called.");
+    
+    //farm the params
+    var shortname = request.params.shortname;
+    var urlData = data.getUrl(shortname);
+    console.log(urlData.url);
+    response.redirect(urlData.url);
+    //TODO: do something useful
+    //response.end("consider yourself redirected to " + redirectUrl + "...");
+    return;
 }
 
 /////////////// REQUEST HANDLERS ///////////////////////
--- urlData.js
+++ urlData.js
@@ -2,24 +2,24 @@
 var urlMap = {};
 
 function getUrl(shortName){
-	console.log("Fetching url for " + shortName);
-	//TODO: update lastHitTime and hits 
+    console.log("Fetching url for " + shortName);
+    //TODO: update lastHitTime and hits 
     return urlMap[shortName];
 }
 
 function addUrl(shortName , url){
-	console.log("Attempting to add url for " + shortName + " : "+url);
-	if(urlMap[shortName]===undefined){
-		urlMap[shortName] = {shortName:shortName , url:url , hits: 1, creationTime:Date.now(), lastHitTime:Date.now()};
-		return true;
-	}else{
-		return false;
-	}
-	
+    console.log("Attempting to add url for " + shortName + " : "+url);
+    if(urlMap[shortName]===undefined){
+        urlMap[shortName] = {shortName:shortName , url:url , hits: 1, creationTime:Date.now(), lastHitTime:Date.now()};
+        return true;
+    }else{
+        return false;
+    }
+    
 } 
 
 function getAll(){
-	return urlMap;
+    return urlMap;
 }
 /////////////// EXPORTS ///////////////////////
 //THESE NEED TO MAP TO EXACTLY TO A REQUEST/POST HANDLER ABOVE
